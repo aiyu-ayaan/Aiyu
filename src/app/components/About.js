2107d29579ecd4dc46901ed226e7a391ea0e714c
aiyu-ayaan <aiyuayaan22@gmail.com>
2025-09-29 01:09:25 +0530

    feat: move skills, experiences, and education data to a separate aboutData file for better organization

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -2,53 +2,9 @@
 "use client";
 import React from 'react';
 import { motion } from 'framer-motion';
+import { skills, experiences, education } from '../data/aboutData';
 
 const About = () => {
-  const skills = [
-    { name: 'Kotlin (Advanced)', level: 90 },
-    { name: 'Java (Intermediate)', level: 70 },
-    { name: 'Python (Intermediate)', level: 60 },
-    { name: 'C# (Basic)', level: 40 },
-    { name: 'JavaScript (Basic)', level: 50 },
-  ];
-
-  
-
-  const experiences = [
-    {
-      company: 'Adrosonic',
-      role: 'Software Engineer',
-      duration: 'Jun 2025 - Present',
-      description: 'Developing enterprise-level applications using WordPress and .NET Framework. Building scalable web applications with modern backend technologies.',
-    },
-    {
-      company: 'Adrosonic',
-      role: 'Software Engineer (Trainee) - Internship',
-      duration: 'Dec 2024 - Jun 2025',
-      description: 'Developed a POC for Dynamics 365 and Instanda integration, reducing process automation time by 40%. Redesigned office website UI/UX, improving load times and user engagement.',
-    },
-    {
-      company: 'BeyondSchool',
-      role: 'Android Developer Intern',
-      duration: 'Jul 2022 - Mar 2023',
-      description: 'Implemented Text-to-Speech and Speech-to-Text functionality, increasing app accessibility. Designed and integrated gamification features, boosting user retention.',
-    },
-  ];
-
-  const education = [
-    {
-      institution: 'Birla Institute of Technology, Mesra',
-      degree: 'Master of Computer Applications',
-      duration: 'Aug 2023 - May 2025',
-      cgpa: '8.1/10.0',
-    },
-    {
-      institution: 'Birla Institute of Technology, Mesra',
-      degree: 'Bachelor of Computer Applications',
-      duration: 'Aug 2019 - May 2022',
-      cgpa: '8.2/10.0',
-    },
-  ];
 
   return (
     <motion.div
