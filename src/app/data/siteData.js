83e6d59b1fe572104e4ecc72e524f5b9b8f068c0
aiyu-ayaan <aiyuayaan22@gmail.com>
2025-09-29 12:55:03 +0530

    feat: refactor Footer component to use dynamic social links from siteData

--- src/app/data/siteData.js
+++ src/app/data/siteData.js
@@ -1,2 +1,27 @@
+import { FaGithub, FaLinkedin, FaInstagram, FaEnvelope } from 'react-icons/fa';
+
 export const homeRoles = ['Android Developer', 'Learner'];
 export const githubLink = "https://github.com/aiyu-ayaan";
+
+export const socials = [
+  {
+    name: 'GitHub',
+    url: 'https://github.com/aiyu-ayaan',
+    icon: FaGithub,
+  },
+  {
+    name: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/aiyu/',
+    icon: FaLinkedin,
+  },
+  {
+    name: 'Instagram',
+    url: 'https://www.instagram.com/aiyu.dev_/',
+    icon: FaInstagram,
+  },
+  {
+    name: 'Email',
+    url: 'mailto:aiyu.ayaan@gmail.com',
+    icon: FaEnvelope,
+  },
+];
