b497e0b5fc8706e688174a5a3b57731f680f5369
aiyu-ayaan <aiyuayaan22@gmail.com>
2026-06-18 02:53:44 +0530

    test(perf): update k6 load test script configuration

--- test.js
+++ test.js
@@ -1,5 +1,33 @@
-import http from "k6/http";
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+    stages: [
+        { duration: '1m', target: 1000 },
+        { duration: '1m', target: 2500 },
+        { duration: '1m', target: 5000 },
+        { duration: '1m', target: 7500 },
+        { duration: '1m', target: 10000 },
+        { duration: '5m', target: 10000 },
+        { duration: '2m', target: 0 },
+    ],
+
+    thresholds: {
+        http_req_failed: [
+            {
+                threshold: 'rate<0.05', // less than 5% failures
+                abortOnFail: true,
+            },
+        ],
+    },
+};
 
 export default function () {
-    http.get("http://localhost:3000");
+    const res = http.get('http://localhost:3000');
+
+    check(res, {
+        'status was 200': (r) => r.status === 200,
+    });
+
+    sleep(1);
 }
